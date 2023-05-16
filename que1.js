f50ba2356f31ed45bfa742f10f6dc3e4fe9ccc41
kiruiaaron <kiruiaaron471@gmail.com>
2023-05-16 09:43:30 +0300

    A js file to sort array of employees in ascending order by age

---
+++ que1.js
@@ -0,0 +1,18 @@
+let employees = [ { 
+    firstName: 'John', 
+    lastName: 'Doe',
+     age: 27, 
+     joinedDate: 'December 15, 2017' }, 
+     
+     { firstName: 'Ana', 
+     lastName: 'Rosy', 
+     age: 25,
+      joinedDate: 'January 15, 2019' }, 
+      
+      { firstName: 'Zion', 
+      lastName: 'Albert', 
+      age: 30, 
+      joinedDate: 'February 15, 2011' } ];
+
+      employees.sort((x,y)=>(x.age < y.age) ? -1 : 1);
+      console.log(employees)
